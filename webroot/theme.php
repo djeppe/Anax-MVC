7b92a20c9aba9a7d4f52410d2f07dff32235256b
djeppe <jesper.norrman@gmail.com>
2014-05-14 16:55:38 +0200

    added typography.html and fixed horizontal grid

--- webroot/theme.php
+++ webroot/theme.php
@@ -5,6 +5,10 @@ require __DIR__.'/config_with_app.php';
 $app->navbar->configure(ANAX_APP_PATH . 'config/navbar-grid.php');
 $app->theme->configure(ANAX_APP_PATH . 'config/theme-grid.php');
 
+if(isset($_GET['show-grid'])) {
+	$app->theme->addStylesheet('css/anax-grid/display_grid.css');
+}
+
 $app->router->add('', function() use ($app) {
     
     $app->theme->setTitle("Nytt tema");
@@ -15,6 +19,19 @@ $app->router->add('', function() use ($app) {
     $app->views->add('welcome/page', [
         'content' => $content,
     ]);
+});
+
+$app->router->add('grid', function() use ($app) {
+	
+	$app->theme->addStylesheet('css/anax-grid/display_grid.css');
+	$app->theme->setTitle("Visa rutnät");
+	
+	$content = $app->fileContent->get('tema.md');
+	$content = $app->textFilter->doFilter($content, 'shortcode, markdown');
+	
+	$app->views->add('welcome/page', [
+		'content' => $content,
+	]);
 });
 
 $app->router->add('regioner', function() use ($app) {
@@ -37,6 +54,34 @@ $app->router->add('regioner', function() use ($app) {
                ->addString('footer-col-4', 'footer-col-4');
  
 });
+
+$app->router->add('typografi', function() use ($app) {
+
+    $app->theme->setTitle("Typografi");
+
+    $content = $app->fileContent->get('typography.html');
+
+    $app->views->add('welcome/page', ['content' => $content]);
+    $app->views->add('welcome/page', ['content' => $content], 'sidebar');
+
+});
+
+$app->router->add('source', function() use ($app) {
+    
+    $app->theme->addStylesheet('css/source.css');
+    $app->theme->setTitle("Källkod");
+    
+    $source = new \Mos\Source\CSource([
+        'secure_dir' => '..',
+        'base_dir' => '..',
+        'add_ignore' => ['.htaccess'],    
+    ]);
+    
+    $app->views->add('me/source', [
+        'content' => $source->View()
+        ]);
+});
+
 
 $app->router->handle();
 $app->theme->render();
